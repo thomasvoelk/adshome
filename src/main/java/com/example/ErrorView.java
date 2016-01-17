e2d6440b7f0c9ac115538380b765df4548664c1f
thomas <thomas.voelk@gmail.com>
2016-01-17 19:01:58 +0100

    house cleaning

--- src/main/java/com/example/ErrorView.java
+++ src/main/java/com/example/ErrorView.java
@@ -1,18 +1,18 @@
 package com.example;
 
 import com.vaadin.navigator.*;
-import com.vaadin.server.*;
 import com.vaadin.ui.*;
 
+@ViewConfig(viewName = ErrorView.VIEW_NAME, messageKey = "view.error")
 public class ErrorView extends VerticalLayout implements View {
+    public static final String VIEW_NAME = "error";
+
     @Override
     public void enter(ViewChangeListener.ViewChangeEvent event) {
         setSizeFull();
-        setMargin(true);
         Label label = new Label(ApplicationUi.getI18N().get("view.error.text"));
         label.addStyleName(AdsTheme.LABEL_FAILURE);
         addComponent(label);
         setComponentAlignment(label, Alignment.MIDDLE_CENTER);
-        Page.getCurrent().setTitle(ApplicationUi.getI18N().get("view.error"));
     }
 }
