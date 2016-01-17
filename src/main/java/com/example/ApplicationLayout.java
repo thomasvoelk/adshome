fb4241fbbdaf11eee5b8c6330295200cc041166d
thomas <thomas.voelk@gmail.com>
2016-01-17 18:09:29 +0100

    removed unnecessary field

--- src/main/java/com/example/ApplicationLayout.java
+++ src/main/java/com/example/ApplicationLayout.java
@@ -4,7 +4,6 @@ import com.vaadin.navigator.*;
 import com.vaadin.spring.annotation.*;
 import com.vaadin.ui.*;
 import org.springframework.beans.factory.annotation.*;
-import org.vaadin.spring.i18n.*;
 
 import javax.annotation.*;
 
@@ -15,12 +14,10 @@ public class ApplicationLayout extends HorizontalLayout implements View {
     public static final String VIEW_NAME = "layout";
     protected Panel content;
     private NavigationBar navigationBar;
-    private I18N i18n;
 
     @Autowired
-    public ApplicationLayout(NavigationBar navigationBar, I18N i18n) {
+    public ApplicationLayout(NavigationBar navigationBar) {
         this.navigationBar = navigationBar;
-        this.i18n = i18n;
     }
 
     @PostConstruct
@@ -30,7 +27,6 @@ public class ApplicationLayout extends HorizontalLayout implements View {
     }
 
     private void initLayouts() {
-
         content = new Panel();
         content.setSizeFull();
         content.addStyleName(AdsTheme.PANEL_BORDERLESS);
